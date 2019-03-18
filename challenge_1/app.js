df9f4a9f124f4c24da8b56f63cc6ae30f326ed09
Nicholas Miron <nicholasmiron@gmail.com>
2019-03-18 11:27:26 -0500

    Have a board on the screen and content can be clicked

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,26 @@
 
+let board = document.getElementById('board');
 
+//Creates a board of a given size
+let populateBoard = function(size) {
+  for (let i = 1; i <= size; i++) {
+    let row = document.createElement('div');
+    row.className = 'row';
+
+    for(let j = 1; j <= size; j++) {
+      let cell = document.createElement('div');
+      cell.className = 'cell';
+      cell.innerText = 'x' + j;
+      cell.addEventListener('click', () => console.log(cell.innerHTML))
+      row.appendChild(cell);
+    }
+
+    board.appendChild(row);
+  }
+  
+}
+
+  // document.getElementsById('board').addEventListener('click', console.log('he'));
+
+populateBoard(3);
+// handleCellClick();
