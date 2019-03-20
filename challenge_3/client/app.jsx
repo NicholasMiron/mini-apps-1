6d0204aa0e8d238038ff1afef1a2429e0fe661b4
Nicholas Miron <nicholasmiron@gmail.com>
2019-03-20 14:40:43 -0500

    Added a bunch of form info

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,8 +1,91 @@
+//Account Creation Form
+function Account(props) {
+  return (
+    <form>
+      <p>
+        <label for='name'>Name: </label> 
+        <input name='name' id='name' type='text'></input>
+      </p>
+      <p>
+        <label for='email'>Email: </label> 
+        <input name='email' id='email' type='text'></input>
+      </p>
+      <p>
+        <label for='password'>Password: </label> 
+        <input name='password' id='password' type='text'></input>
+      </p>
+      <input type='submit'></input>
+    </form>
+  )
+}
+
+//Shipping Info Form
+function Shipping(props) {
+  return (
+    <form>
+      <p>
+        <label for='address1'>Address Line 1:</label>
+        <input name='address1' id='address1' type='text'></input>
+      </p>
+      <p>
+        <label for='address2'>Address Line 2:</label>
+        <input name='address2' id='address2' type='text'></input>
+      </p>
+      <p>
+        <label for='city'>City:</label>
+        <input name='city' id='city' type='text'></input>
+      </p>
+      <p>
+        <label for='state'>State:</label>
+        <input name='state' id='state' type='text'></input>
+      </p>
+      <p>
+        <label for='zipcode'>Zipcode:</label>
+        <input name='zipcode' id='zipcode' type='text'></input>
+      </p>
+      <p>
+        <label for='phone'>Phone Number:</label>
+        <input name='phone' id='phone' type='text'></input>
+      </p>
+      <input type='submit'></input>
+    </form>
+  )
+}
+
+//Payment Form
+function Payment(props) {
+  return (
+    <form>
+      <p>
+        <label for='card'>Card:</label>
+        <input name='card' id='card' type='text'></input>
+      </p>
+      <p>
+        <label for='expiration'>Expiration Date:</label>
+        <input name='expiration' id='expiration' type='text'></input>
+      </p>
+      <p>
+        <label for='cvv'>CVV:</label>
+        <input name='cvv' id='cvv' type='text'></input>
+      </p>
+      <p>
+        <label for='billzip'>Billing Zipcode:</label>
+        <input name='billzip' id='billzip' type='text'></input>
+      </p>
+      <p>
+        <input type='submit'></input>
+      </p>
+    </form>
+  )
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      accountOn:false,
+      shippingOn:false,
+      paymentOn:false
     }
   }
   
@@ -14,6 +97,9 @@ class App extends React.Component {
     return(
       <>
         <button onClick={this.handleCheckoutClick}>Checkout</button>
+        <Account />
+        <Shipping />
+        <Payment />
       </>
     )
   }
---
+++ challenge_3/public/account.js
@@ -0,0 +1,8 @@
+export default function Account(props) {
+  return React.createElement("form", null, React.createElement("input", {
+    type: "text"
+  }), React.createElement("input", {
+    type: "submit"
+  }));
+}
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL2NsaWVudC9hY2NvdW50LmpzeCJdLCJuYW1lcyI6WyJBY2NvdW50IiwicHJvcHMiXSwibWFwcGluZ3MiOiJBQUFBLGVBQWUsU0FBU0EsT0FBVCxDQUFpQkMsS0FBakIsRUFBd0I7QUFDckMsU0FDRSxrQ0FDRTtBQUFPLElBQUEsSUFBSSxFQUFDO0FBQVosSUFERixFQUVFO0FBQU8sSUFBQSxJQUFJLEVBQUM7QUFBWixJQUZGLENBREY7QUFNRCIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIEFjY291bnQocHJvcHMpIHtcbiAgcmV0dXJuIChcbiAgICA8Zm9ybT5cbiAgICAgIDxpbnB1dCB0eXBlPSd0ZXh0Jz48L2lucHV0PlxuICAgICAgPGlucHV0IHR5cGU9J3N1Ym1pdCc+PC9pbnB1dD5cbiAgICA8L2Zvcm0+XG4gIClcbn1cblxuIl19
